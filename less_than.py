41627950896719bca61ca8ed57c649145dff2a79
Vireak Yim <vydg5@mail.missouri.edu>
2016-02-23 18:04:46 -0600

    Exercise 3

---
+++ less_than.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 22 21:30:37 2016
+
+@author: Vireak
+"""
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+c = int(input("Enter a number:"))
+b = []
+[b.append(a[i]) if a[i] < c else 0 for i in range(0,len(a))]    
+print("The following number(s): " + str(b) + " are from list a and are smaller than " + str(c))
+
+    
+    
