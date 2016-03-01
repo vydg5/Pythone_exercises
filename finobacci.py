c8b9da5bcc74df7d36b87c3c1d58db7192e464ad
vydg5 <vydg5@mail.missouri.edu>
2016-03-01 14:33:58 -0600

    Fibonacci

--- finobacci.py
+++ finobacci.py
@@ -10,6 +10,7 @@ def fibonacci(number):
 
     for i in range(2,number):
         b[i] = b[i-1]+b[i-2]
+        print(b[i-2])
     return print("Here is your Fibonacci list: " + str(b))
         
 a = int(input("How many Fibonnaci do you need? "))
